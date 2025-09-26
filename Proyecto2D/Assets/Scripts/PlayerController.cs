704710faeca01e247cc7a268684bd3867fc1929f
dmoyavilch <dmoyavilch1@educacion.navarra.es>
2025-09-26 18:22:49 +0200

    Sprites, animaciones y background

--- Proyecto2D/Assets/Scripts/PlayerController.cs
+++ Proyecto2D/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float speed;
+    public float jumpforce;
+
+
+    private Transform transform;
+    private Rigidbody2D rb;
+    private Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Movement();
+
+
+
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 }
